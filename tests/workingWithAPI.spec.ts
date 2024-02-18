136eafe49b611210939020e0e0efe18e863be9fe
minhvo <minhvo4398@gmail.com>
2024-02-18 14:07:59 +0700

    mock api

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,11 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import tags from "../test-data/test.json";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://angular.realworld.how/');
-})
+  // thay vi parse url(https://angular.realworld.how/) thi co the viet gon voi pattern nhu duoi
+  await page.route("*/**/api/tags", async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
 
-test('has title', async ({ page }) => {
-  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
+  await page.goto("https://angular.realworld.how/");
 });
 
-
+test("has title", async ({ page }) => {
+  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
+  await page.waitForTimeout(5000);
+});
