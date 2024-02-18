6a201f66eacd86930ce65eba6cd97f7ea3595eb4
minhvo <minhvo4398@gmail.com>
2024-02-18 16:13:24 +0700

    modify API response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import tags from "../test-data/test.json";
+import { join } from "path";
 
 test.beforeEach(async ({ page }) => {
   // thay vi parse url(https://angular.realworld.how/) thi co the viet gon voi pattern nhu duoi
@@ -9,10 +10,27 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  await page.route("*/**/api/articles*", async (route) => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = "This is a test title";
+    responseBody.articles[0].description = "This is a description";
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody),
+    });
+  });
+
   await page.goto("https://angular.realworld.how/");
 });
 
 test("has title", async ({ page }) => {
   await expect(page.locator(".navbar-brand")).toHaveText("conduit");
+  await expect(page.locator("app-article-list h1").first()).toContainText(
+    "This is a test title"
+  );
+  await expect(page.locator("app-article-list p").first()).toContainText(
+    "This is a description"
+  );
   await page.waitForTimeout(5000);
 });
