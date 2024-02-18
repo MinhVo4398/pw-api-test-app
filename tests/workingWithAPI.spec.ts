d0d9816a0c62bb9d4ddb0918f6e2b9a1f6bb6337
minhvo <minhvo4398@gmail.com>
2024-02-18 17:09:11 +0700

    perform api call

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, request } from "@playwright/test";
 import tags from "../test-data/test.json";
 import { join } from "path";
 
@@ -10,27 +10,72 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  await page.goto("https://angular.realworld.how/");
+  await page.getByText("Sign in").click();
+  await page.getByRole("textbox", {name: "Email"}).fill("pwtestminh@test.com");
+  await page.getByRole("textbox", {name: "Password"}).fill("123456")
+  await page.getByRole("button").click();
+});
+
+test("has title", async ({ page }) => {
   await page.route("*/**/api/articles*", async (route) => {
     const response = await route.fetch();
     const responseBody = await response.json();
-    responseBody.articles[0].title = "This is a test title";
-    responseBody.articles[0].description = "This is a description";
+    responseBody.articles[0].title = "This is a MOCK test title";
+    responseBody.articles[0].description = "This is a MOCK description";
 
     await route.fulfill({
       body: JSON.stringify(responseBody),
     });
   });
 
-  await page.goto("https://angular.realworld.how/");
-});
+  await page.getByText("Global Feed").click();
 
-test("has title", async ({ page }) => {
   await expect(page.locator(".navbar-brand")).toHaveText("conduit");
   await expect(page.locator("app-article-list h1").first()).toContainText(
-    "This is a test title"
+    "This is a MOCK test title"
   );
   await expect(page.locator("app-article-list p").first()).toContainText(
-    "This is a description"
+    "This is a MOCK description"
   );
   await page.waitForTimeout(5000);
+});
+
+test("Delete article", async ({ page, request }) => {
+  // API to login
+  const response = await request.post(
+    "https://api.realworld.io/api/users/login",
+    {
+      data: {
+        user: { email: "pwtestminh@test.com", password: "123456" },
+      },
+    }
+  );
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+  console.log(responseBody.user.token);
+  
+  // API create new article
+ const articleResponse = await request.post("https://api.realworld.io/api/articles", {
+    data: {
+      article: {
+        title: "This is a test article",
+        description: "This is a test description",
+        body: "This is a test body",
+        tagList: [],
+      },
+    },
+    headers: {
+      Authorization: `Token ${accessToken}`,
+    },
+  });
+  expect(articleResponse.status()).toEqual(201); 
+
+  //  delete article 
+  await page.getByText("Global Feed").click();
+  await page.getByText("This is a test article").click();
+  await page.getByRole("button", {name:"Delete Article"}).first().click();
+  await page.getByText("Global Feed").click();
+  await expect(page.locator("app-article-list h1").first()).not.toContainText("This is a test article");
+  
 });
